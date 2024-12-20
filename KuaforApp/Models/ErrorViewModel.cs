0ad1b3302fd5b2fab2e370c3575a3bbde1236231
Mustafa Erdoğan <mustafaerdoganu@gmail.com>
2024-12-20 14:34:09 +0300

    MVC Projesi

---
+++ KuaforApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KuaforApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

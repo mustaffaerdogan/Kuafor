2fa321d8186191d4ff16ad567ebd3d1b949a199c
Mustafa Erdoğan <mustafaerdoganu@gmail.com>
2024-12-21 23:38:35 +0300

    İşletmeler eklendi

---
+++ KuaforApp/Views/Isletmeler/Isletmeler.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KuaforApp.Views.Isletmeler
+{
+    public class IsletmelerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

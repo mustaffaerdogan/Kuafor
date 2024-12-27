7a3457360013c40bdd1670b728dcc2733d5223d3
Ali Aydın <aliaydin03489@gmail.com>
2024-12-27 14:39:45 +0300

    AppoinmentController.cs duzenlendi

--- KuaforApp/Controllers/AppoinmentController.cs
+++ KuaforApp/Controllers/AppoinmentController.cs
@@ -1,37 +1,120 @@
-﻿using System.Diagnostics;
-using KuaforApp.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YourNamespace.Models;
 
-namespace KuaforApp.Controllers
+namespace YourNamespace.Controllers
 {
-    public class HomeController : Controller
+    public class AppointmentController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext db = new ApplicationDbContext();
 
-        public HomeController(ILogger<HomeController> logger)
+        // GET: Appointment
+        public ActionResult Index()
         {
-            _logger = logger;
+            return View(db.Appointments.ToList());
         }
 
-        // Allow anonymous access to the landing page
-        [AllowAnonymous]
-        public IActionResult Index()
+        // GET: Appointment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointment);
+        }
+
+        // GET: Appointment/Create
+        public ActionResult Create()
         {
             return View();
         }
 
-        // Require authentication for the dashboard
-        [Authorize]
-        public IActionResult Dashboard()
+        // POST: Appointment/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CustomerName,Date,Time,Service")] Appointment appointment)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                db.Appointments.Add(appointment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(appointment);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        // GET: Appointment/Edit/5
+        public ActionResult Edit(int? id)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointment);
+        }
+
+        // POST: Appointment/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CustomerName,Date,Time,Service")] Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(appointment).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(appointment);
+        }
+
+        // GET: Appointment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointment);
+        }
+
+        // POST: Appointment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Appointment appointment = db.Appointments.Find(id);
+            db.Appointments.Remove(appointment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
